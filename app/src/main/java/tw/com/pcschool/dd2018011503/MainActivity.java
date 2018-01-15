e4a680bd9a01db2671191d872b686c330820f8e7
Teacher <shaojung@gmail.com>
2018-01-15 15:19:52 +0800

    Initial commit

---
+++ app/src/main/java/tw/com/pcschool/dd2018011503/MainActivity.java
@@ -0,0 +1,35 @@
+package tw.com.pcschool.dd2018011503;
+
+import android.content.SharedPreferences;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void clickWrite(View v)
+    {
+        SharedPreferences sp = getSharedPreferences("mydata", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        EditText ed = (EditText) findViewById(R.id.editText);
+        editor.putString("data1", ed.getText().toString());
+        editor.commit();
+    }
+    public void clickRead(View v)
+    {
+        SharedPreferences sp = getSharedPreferences("mydata", MODE_PRIVATE);
+        String str = sp.getString("data1", "");
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText(str);
+    }
+}
